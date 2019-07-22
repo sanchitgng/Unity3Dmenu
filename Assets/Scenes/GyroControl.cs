44013aeabdae1a4458f1bcdc0a8e4394a8fb8b4c
Sanchit Sharma <sanchit.sharma99@gmail.com>
2019-07-22 17:58:16 +0530

    3D menu as showcased | standard assests not commited | google VR not commited.

---
+++ Assets/Scenes/GyroControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+    private bool GyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        GyroEnabled = enableGyro();
+    }
+
+    private bool enableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rotation = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rotation;
+        }
+    }
+}
---
+++ Assets/cinematics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class cinematics : MonoBehaviour
+{
+    public void cine()
+    {
+        GetComponent<PlayableDirector>().Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
